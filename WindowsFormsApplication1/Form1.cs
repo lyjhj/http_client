357997d445aba299b8db58dd7b63bf427851b520
lyjhj <26431316@qq.com>
2018-05-25 23:08:33 +0800

    new

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // <summary>
+        /// 将本地文件上传到指定的服务器(HttpWebRequest方法)
+        /// </summary>
+        /// <param name="address">文件上传到的服务器</param>
+        /// <param name="fileNamePath">要上传的本地文件（全路径）</param>
+        /// <param name="saveName">文件上传后的名称</param>
+        /// <param name="progressBar">上传进度条</param>
+        /// <returns>成功返回1，失败返回0</returns>
+        private void UploadRequest(string url, string filePath)
+        {
+            // 时间戳，用做boundary
+            string timeStamp = DateTime.Now.Ticks.ToString("x");
+
+            //根据uri创建HttpWebRequest对象
+            HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create(new Uri(url));
+            httpReq.Method = "POST";
+            httpReq.AllowWriteStreamBuffering = false; //对发送的数据不使用缓存
+            httpReq.Timeout = 300000;  //设置获得响应的超时时间（300秒）
+            httpReq.ContentType = "multipart/form-data; boundary=" + timeStamp;
+
+            //文件
+            FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+
+            //头信息
+            string boundary = "--" + timeStamp;
+            string dataFormat = boundary + "\r\nContent-Disposition: form-data; name=\"{0}\";filename=\"{1}\"\r\nContent-Type:application/octet-stream\r\n\r\n";
+            string header = string.Format(dataFormat, "my_file", Path.GetFileName(filePath));
+            byte[] postHeaderBytes = Encoding.UTF8.GetBytes(header);
+
+            //结束边界
+            byte[] boundaryBytes = Encoding.ASCII.GetBytes("\r\n--" + timeStamp + "--\r\n");
+
+            long length = fileStream.Length + postHeaderBytes.Length + boundaryBytes.Length;
+
+            httpReq.ContentLength = length;//请求内容长度
+
+            try
+            {
+                //每次上传4k
+                int bufferLength = 4096;
+                byte[] buffer = new byte[bufferLength];
+
+                //已上传的字节数
+                long offset = 0;
+                int size = binaryReader.Read(buffer, 0, bufferLength);
+                Stream postStream = httpReq.GetRequestStream();
+
+                //发送请求头部消息
+                postStream.Write(postHeaderBytes, 0, postHeaderBytes.Length);
+
+                while (size > 0)
+                {
+                    postStream.Write(buffer, 0, size);
+                    offset += size;
+                    size = binaryReader.Read(buffer, 0, bufferLength);
+                }
+
+                //添加尾部边界
+                postStream.Write(boundaryBytes, 0, boundaryBytes.Length);
+                postStream.Close();
+
+                //获取服务器端的响应
+                using (HttpWebResponse response = (HttpWebResponse)httpReq.GetResponse())
+                {
+                    Stream receiveStream = response.GetResponseStream();
+                    StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+                    string returnValue = readStream.ReadToEnd();
+                    MessageBox.Show(returnValue);
+                    response.Close();
+                    readStream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = "error";
+                //Debug.WriteLine("文件传输异常： " + ex.Message);
+            }
+            finally
+            {
+                fileStream.Close();
+                binaryReader.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string address = "http:" + "//" + "10.63.6.112:8888" + "/" + "upload" + "/";
+            string fileNamePath = "d:" + "\\" + "test.txt";
+            string saveName = "test.txt";
+            UploadRequest(address, fileNamePath);
+        }
+
+
+
+    }
+}
+
